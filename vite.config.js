ce1514ae9f507d7b7987f3a6b3258049d35349f7
Pratyush Mehta <pratyushmehta@Pratyushs-MacBook-Air.local>
2025-06-12 00:00:05 +0530

    Fix: Install typewriter-effect and update external modules

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
     rollupOptions: {
-      external: ['@tsparticles/react', 'tsparticles', 'react-icons'],
+      external: [
+        '@tsparticles/react',
+        'tsparticles',
+        'react-icons',
+        'typewriter-effect',
+      ],
     },
   },
 })
